cbb4827198b03b74c14af761d666f78e1e5f440e
Stuart MacKay <smackay@flagstonesoftware.com>
2024-11-19 20:06:06 +0000

    refactor: Add the location of the .env file to the settings

--- src/main/settings.py
+++ src/main/settings.py
@@ -5,6 +5,8 @@ from pathlib import Path
 
 # The root directory of the project.
 ROOT_DIR = Path(__file__).parent.parent.parent.absolute()
+# The location of the .env dile
+DOTENV_FILE = ROOT_DIR.joinpath(".env")
 # The directory for data
 DATA_DIR = ROOT_DIR.joinpath("data")
 # The directory where the eBird Basic Dataset files are downloaded to..
