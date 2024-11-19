2edb18c60d0881a9c29080033a0290af876b5e84
Stuart MacKay <smackay@flagstonesoftware.com>
2024-11-19 20:06:39 +0000

    refactor: Simplify the load script

--- scripts/load.py
+++ scripts/load.py
@@ -7,21 +7,12 @@ scheduler such as cron.
 """
 import os
 
-from pathlib import Path
-
 from dotenv import load_dotenv
 
-# Get the root directory of the project
-root_dir =Path(__file__).absolute().parent.parent
-
-# Now we can load the local python
 from main import settings
 from main.db import loaders
 
-# Change to the root directory
-os.chdir(root_dir)
-# Load the environment variables from the .env file
-load_dotenv()
+load_dotenv(dotenv_path=settings.DOTENV_FILE)
 
 api_key = os.getenv("EBIRD_API_KEY")
 region = os.getenv("EBIRD_API_REGION")
