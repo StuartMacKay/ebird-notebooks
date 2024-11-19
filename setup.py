8ef9aa74f4c599e139dc29b8e7b8038d1ceeafdb
Stuart MacKay <smackay@flagstonesoftware.com>
2024-11-19 19:56:55 +0000

    build: Use setuptools to install main as an editable package

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""
+setup.py
+
+setup is used to create an editable package for the python code in the
+src directory. It can be imported just like any other package, so we
+avoid any steps to update the PYTHONPATH at the start of a notebook.
+
+"""
+from setuptools import setup
+
+
+setup(
+    name="ebird-notebooks",
+    version="0.0.0",
+    description="Jupyter notebooks for analysing eBird checklists",
+    packages=["main"],
+    package_dir={"": "src"},
+)
