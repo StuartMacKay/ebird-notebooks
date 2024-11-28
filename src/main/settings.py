ff771e8dc6c8afb2f9a511f55108ad900af3f770
Stuart MacKay <smackay@flagstonesoftware.com>
2024-11-28 09:15:26 +0000

    refactor: Remove Basic Dataset database url from the settings

--- src/main/settings.py
+++ src/main/settings.py
@@ -15,6 +15,4 @@ DOWNLOAD_DIR = DATA_DIR.joinpath("downloads")
 # The directory where the database files are located.
 DATABASE_DIR = DATA_DIR.joinpath("databases")
 # The URL used by the database engine.
-EBD_DATABASE_URL = f"sqlite+pysqlite:///{DATABASE_DIR}/ebd.sqlite3"
-# The URL used by the database engine.
 API_DATABASE_URL = f"sqlite+pysqlite:///{DATABASE_DIR}/api.sqlite3"
