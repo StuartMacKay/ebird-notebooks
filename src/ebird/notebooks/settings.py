45952cf7b12ac345309da15a1fe8d606ba38f234
Stuart MacKay <smackay@flagstonesoftware.com>
2024-12-10 09:20:34 +0000

    lint: Add type hints for settings

--- src/ebird/notebooks/settings.py
+++ src/ebird/notebooks/settings.py
@@ -8,21 +8,21 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 # The root directory of the project.
-ROOT_DIR = Path(__file__).parent.parent.parent.parent.absolute()
+ROOT_DIR: Path = Path(__file__).parent.parent.parent.parent.absolute()
 # The location of the .env dile
-DOTENV_FILE = ROOT_DIR.joinpath(".env")
+DOTENV_FILE: Path = ROOT_DIR.joinpath(".env")
 # The directory for data
-DATA_DIR = ROOT_DIR.joinpath("data")
+DATA_DIR: Path = ROOT_DIR.joinpath("data")
 # The directory where the eBird Basic Dataset files are downloaded to..
-DOWNLOAD_DIR = DATA_DIR.joinpath("downloads")
+DOWNLOAD_DIR: Path = DATA_DIR.joinpath("downloads")
 # The directory where the database files are located.
-DATABASE_DIR = DATA_DIR.joinpath("databases")
+DATABASE_DIR: Path = DATA_DIR.joinpath("databases")
 
 # Load the environment variables. See .env.example for a full description.
 load_dotenv(DOTENV_FILE)
 # The key needed to access the eBird API.
-API_KEY = os.getenv("API_KEY")
+API_KEY: str = os.getenv("API_KEY")
 # The regions for which to load observations for.
-API_REGIONS = os.getenv("API_REGIONS")
+API_REGIONS: str = os.getenv("API_REGIONS")
 # The number of days to fetch checklists for.
-API_PAST_DAYS = int(os.getenv("API_PAST_DAYS", "5"))
+API_PAST_DAYS: int = int(os.getenv("API_PAST_DAYS", "5"))
