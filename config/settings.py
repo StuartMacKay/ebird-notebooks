afafcfd5ca74264759741daa05e4f343bce86187
Stuart MacKay <smackay@flagstonesoftware.com>
2025-07-13 10:43:31 +0100

    Project - Remove unneeded dependency, django-debug-toolbar

--- config/settings.py
+++ config/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_extensions",
-    "debug_toolbar",
     "checklists",
 ]
 
--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.urls import include, path
+from django.urls import path
 
-import debug_toolbar
 
 urlpatterns = [
-    # Change the path to the Django Admin to something non-standard.
     path("admin/", admin.site.urls),  # type: ignore
-    path("__debug__/toolbar/", include(debug_toolbar.urls)),
 ] + staticfiles_urlpatterns()
