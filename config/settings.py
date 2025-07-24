d47df8feb961b7f3eb848c6b42d17ac4a67d8291
Stuart MacKay <smackay@flagstonesoftware.com>
2025-07-24 17:37:16 +0100

    Settings - Remove trace of django-debug-toolbar

--- config/settings.py
+++ config/settings.py
@@ -57,7 +57,6 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 # ##############
